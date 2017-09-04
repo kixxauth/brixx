9e638e6dea01aab0373abf2523969cbd09c0d1ea
Kris Walker <kris@kixx.name>
2017-09-04 09:47:45 -0400

    New linting with eslint
     Changes to be committed:
    	new file:   .eslintrc.js
    	modified:   index.js
    	modified:   package.json
    	modified:   test/test.js

--- index.js
+++ index.js
@@ -1,92 +1,68 @@
-/* global define */
-;(function (global, factory) {
-  'use strict';
+/* globals define, module */
+;(function (root, factory) { // eslint-disable-line no-extra-semi
+	'use strict';
 
-  // Support CommonJS, AMD, and global script loading.
-  if (typeof exports === 'object' && typeof module !== 'undefined') {
-    factory(exports);
-  } else if (typeof define === 'function' && define.amd) {
-    define(factory);
-  } else {
-    global.BRIXX = factory();
-  }
+	// Support CommonJS, AMD, and global script loading.
+	if (typeof define === 'function' && define.amd) {
+		define(['brixx'], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		factory(module.exports);
+	} else {
+		root.BRIXX = {};
+		factory(root.BRIXX);
+	}
 }(this, function (BRIXX) {
-  'use strict';
-  BRIXX = BRIXX || {};
+	'use strict';
 
-  //
-  // Utilities
-  //
+	//
+	// Utilities
+	//
 
-  function noop() {}
+	function noop() {}
+	BRIXX.noop = noop;
 
-  function extend(target, source) {
-    return Object.keys(source).reduce(function (target, key) {
-      target[key] = source[key];
-      return target;
-    }, target);
-  }
+	// Ensures the passed in object is, in fact, an Object.
+	// When `null` or `undefined` are passed in, ensure() returns a new Object
+	// created with `Object.create(null)`. Otherwise it returns the
+	// passed in Object.
+	function ensure(obj) {
+		return obj == void 0 ? {} : obj; // eslint-disable-line eqeqeq
+	}
+	BRIXX.ensure = ensure;
 
-  // Ensures the passed in object is, in fact, an Object.
-  // When `null` or `undefined` are passed in, ensure() returns a new Object
-  // created with `Object.create(null)`. Otherwise it returns the
-  // passed in Object.
-  function ensure(obj) {
-    return (obj == void 0) ? Object.create(null) : obj;
-  }
-  BRIXX.ensure = ensure;
+	// Calls `Object.freeze()` recursively on the passed in Object.
+	// deepFreeze() will skip the `arguemnts`, `caller`, `callee` and `prototype`
+	// properties of a Function. deepFreeze() will throw if passed null or
+	// undefined just like `Object.freeze()` would.
+	function deepFreeze(obj) {
+		Object.freeze(obj);
+		Object.getOwnPropertyNames(obj).forEach(function (key) {
+			if (typeof obj === 'function' &&
+					(key === 'arguments' ||
+						key === 'caller' ||
+						key === 'callee' ||
+						key === 'prototype')) {
+				return;
+			}
 
+			var prop = obj[key];
+			if (prop !== null &&
+					(typeof prop === 'object' || typeof prop === 'function')) {
+				deepFreeze(prop);
+			}
+		});
+		return obj;
+	}
+	BRIXX.deepFreeze = deepFreeze;
 
-  // Calls `Object.freeze()` recursively on the passed in Object.
-  // deepFreeze() will skip the `arguemnts`, `caller`, `callee` and `prototype`
-  // properties of a Function. deepFreeze() will throw if passed null or
-  // undefined just like `Object.freeze()` would.
-  function deepFreeze(obj) {
-    Object.freeze(obj);
-    Object.getOwnPropertyNames(obj).forEach(function (key) {
-      if ( typeof obj === 'function' &&
-           ( key === 'arguments' ||
-             key === 'caller' ||
-             key === 'callee' ||
-             key === 'prototype')) {
-        return;
-      }
+	// Check to see if the passed in Object exists.
+	// Returns false for null, undefined, or NaN.
+	// Returns true for everything else.
+	function exists(obj) {
+		// Because isNaN({}) == true
+		return !(obj == void 0 || typeof obj == 'number' && isNaN(obj)); // eslint-disable-line eqeqeq
+	}
+	BRIXX.exists = exists;
 
-      var prop = obj[key];
-      if ( prop !== null &&
-           (typeof prop === 'object' || typeof prop === 'function')) {
-        deepFreeze(prop);
-      }
-    });
-    return obj;
-  }
-  BRIXX.deepFreeze = deepFreeze;
-
-
-  // Check to see if the passed in Object exists.
-  // Returns false for null, undefined, or NaN.
-  // Returns true for everything else.
-  function exists(obj) {
-    // Because isNaN({}) == true
-    return !(obj == void 0 || (typeof obj == 'number' && isNaN(obj)));
-  }
-  BRIXX.exists = exists;
-
-
-  // A different way to stringify an Object, other than .toString().
-  // 1) Returns an empty string for null, undefined or NaN.
-  // 2) Returns the special '[object Function]' String for Functions.
-  // 3) Returns the result of .toString() for anything else if it exists.
-  // 4) Returns the result of Object.prototype.toString if .toString()
-  //    is not present.
-  function stringify(obj) {
-    // Because isNaN({}) == true
-    if (obj == void 0 || (typeof obj == 'number' && isNaN(obj))) return '';
-    if (typeof obj == 'function') return '[object Function]';
-    if (typeof obj.toString == 'function') return obj.toString();
-    return Object.prototype.toString.call(obj);
-  }
-  BRIXX.stringify = stringify;
-
-  return BRIXX;
+	return BRIXX;
 }));
--- test/test.js
+++ test/test.js
@@ -1,178 +1,135 @@
-var
-expect = require('expect.js'),
-BRIXX  = require('../index');
-
+/* globals BRIXX, describe, it, xit, expect */
 describe('.ensure()', function () {
-  'use strict';
+	'use strict';
 
-  it('returns a blank object if the given value is undefined', function () {
-    var x = BRIXX.ensure();
-    expect(x).to.be.an('object');
-    expect(Object.getPrototypeOf(x)).to.be(null);
-  });
-  it('returns a blank object if the given value is null', function () {
-    var x = BRIXX.ensure(null);
-    expect(x).to.be.an('object');
-    expect(Object.getPrototypeOf(x)).to.be(null);
-  });
-  it('returns false when given false', function () {
-    var x = BRIXX.ensure(false);
-    expect(x).to.be.a('boolean');
-    expect(x).to.be(false);
-  });
-  it('returns 0 when given 0', function () {
-    var x = BRIXX.ensure(0);
-    expect(x).to.be.a('number');
-    expect(x).to.be(0);
-  });
-  it('returns NaN when given NaN', function () {
-    var x = BRIXX.ensure(NaN);
-    expect(x).to.be.a('number');
-    expect(isNaN(NaN)).to.be.ok();
-  });
-  it('returns an empty string when given an empty string', function () {
-    var x = BRIXX.ensure('');
-    expect(x).to.be.a('string');
-    expect(x).to.be('');
-  });
-  it('returns a function when given a function', function () {
-    var
-    f = function () {},
-    x = BRIXX.ensure(f);
-    expect(x).to.be.a('function');
-    expect(x).to.be(f);
-  });
+	it('returns a blank object if the given value is undefined', function () {
+		var x = BRIXX.ensure();
+		expect(x).to.be.an('object');
+		expect(Object.getPrototypeOf(x)).to.be(null);
+	});
+	it('returns a blank object if the given value is null', function () {
+		var x = BRIXX.ensure(null);
+		expect(x).to.be.an('object');
+		expect(Object.getPrototypeOf(x)).to.be(null);
+	});
+	it('returns false when given false', function () {
+		var x = BRIXX.ensure(false);
+		expect(x).to.be.a('boolean');
+		expect(x).to.be(false);
+	});
+	it('returns 0 when given 0', function () {
+		var x = BRIXX.ensure(0);
+		expect(x).to.be.a('number');
+		expect(x).to.be(0);
+	});
+	it('returns NaN when given NaN', function () {
+		var x = BRIXX.ensure(NaN);
+		expect(x).to.be.a('number');
+		expect(isNaN(NaN)).to.be.ok();
+	});
+	it('returns an empty string when given an empty string', function () {
+		var x = BRIXX.ensure('');
+		expect(x).to.be.a('string');
+		expect(x).to.be('');
+	});
+	it('returns a function when given a function', function () {
+		var f = function () {};
+		var x = BRIXX.ensure(f);
+		expect(x).to.be.a('function');
+		expect(x).to.be(f);
+	});
 });
 
 describe('.deepFreeze()', function () {
-  'use strict';
+	'use strict';
 
-  it('throws when passed undefined', function () {
-    expect(function () {
-      BRIXX.deepFreeze();
-    }).to.throwError(function (e) {
-      expect(e).to.be.a(TypeError);
-    });
-  });
+	it('throws when passed undefined', function () {
+		expect(function () {
+			BRIXX.deepFreeze();
+		}).to.throwError(function (e) {
+			expect(e).to.be.a(TypeError);
+		});
+	});
 
-  // Node.js no longer throws for these primitives.
-  xit('throws when passed null', function () {
-    expect(function () {
-      BRIXX.deepFreeze(null);
-    }).to.throwError(function (e) {
-      expect(e).to.be.a(TypeError);
-    });
-  });
-  xit('throws when passed a Boolean', function () {
-    expect(function () {
-      BRIXX.deepFreeze(true);
-      console.log('NO ERROR');
-    }).to.throwError(function (e) {
-      console.log('ERROR:');
-      console.log(e);
-      expect(e).to.be.a(TypeError);
-    });
-  });
-  xit('throws when passed a String', function () {
-    expect(function () {
-      BRIXX.deepFreeze('foo');
-    }).to.throwError(function (e) {
-      expect(e).to.be.a(TypeError);
-    });
-  });
-  xit('throws when passed a Number', function () {
-    expect(function () {
-      BRIXX.deepFreeze(99);
-    }).to.throwError(function (e) {
-      expect(e).to.be.a(TypeError);
-    });
-  });
+	// Node.js no longer throws for these primitives.
+	xit('throws when passed null', function () {
+		expect(function () {
+			BRIXX.deepFreeze(null);
+		}).to.throwError(function (e) {
+			expect(e).to.be.a(TypeError);
+		});
+	});
+	xit('throws when passed a Boolean', function () {
+		expect(function () {
+			BRIXX.deepFreeze(true);
+		}).to.throwError(function (e) {
+			expect(e).to.be.a(TypeError);
+		});
+	});
+	xit('throws when passed a String', function () {
+		expect(function () {
+			BRIXX.deepFreeze('foo');
+		}).to.throwError(function (e) {
+			expect(e).to.be.a(TypeError);
+		});
+	});
+	xit('throws when passed a Number', function () {
+		expect(function () {
+			BRIXX.deepFreeze(99);
+		}).to.throwError(function (e) {
+			expect(e).to.be.a(TypeError);
+		});
+	});
 
-  it('deeply freezes all objects', function () {
-    var
-    fixture = {
-      foo: 'bar',
-      a: [{}, {}, null],
-      b: [1,2,3],
-      x: {foo: {}, bar: 'foo'},
-      z: null
-    };
+	it('deeply freezes all objects', function () {
+		var fixture = {
+			foo: 'bar',
+			a: [{}, {}, null],
+			b: [1,2,3],
+			x: {foo: {}, bar: 'foo'},
+			z: null
+		};
 
-    BRIXX.deepFreeze(fixture);
+		BRIXX.deepFreeze(fixture);
 
-    expect(Object.isFrozen(fixture)).to.be.ok();
-    expect(Object.isFrozen(fixture.a)).to.be.ok();
-    expect(Object.isFrozen(fixture.a[0])).to.be.ok();
-    expect(Object.isFrozen(fixture.a[1])).to.be.ok();
-    expect(Object.isFrozen(fixture.b)).to.be.ok();
-    expect(Object.isFrozen(fixture.x)).to.be.ok();
-    expect(Object.isFrozen(fixture.x.foo)).to.be.ok();
-  });
+		expect(Object.isFrozen(fixture)).to.be.ok();
+		expect(Object.isFrozen(fixture.a)).to.be.ok();
+		expect(Object.isFrozen(fixture.a[0])).to.be.ok();
+		expect(Object.isFrozen(fixture.a[1])).to.be.ok();
+		expect(Object.isFrozen(fixture.b)).to.be.ok();
+		expect(Object.isFrozen(fixture.x)).to.be.ok();
+		expect(Object.isFrozen(fixture.x.foo)).to.be.ok();
+	});
 });
 
 describe('.exists()', function () {
-  'use strict';
+	'use strict';
 
-  it('returns false when passed undefined', function () {
-    expect(BRIXX.exists()).to.be(false);
-  });
-  it('returns false when passed null', function () {
-    expect(BRIXX.exists(null)).to.be(false);
-  });
-  it('returns false when passed NaN', function () {
-    expect(BRIXX.exists(NaN)).to.be(false);
-  });
-  it('returns true when passed false', function () {
-    expect(BRIXX.exists(false)).to.be(true);
-  });
-  it('returns true when passed 0', function () {
-    expect(BRIXX.exists(0)).to.be(true);
-  });
-  it('returns true when passed ""', function () {
-    expect(BRIXX.exists('')).to.be(true);
-  });
-  it('returns true when passed function() {}', function () {
-    expect(BRIXX.exists(function () {})).to.be(true);
-  });
-  it('returns true when passed an Object', function () {
-    expect(BRIXX.exists({})).to.be(true);
-  });
-  it('returns true when passed an Array', function () {
-    expect(BRIXX.exists([])).to.be(true);
-  });
-});
-
-describe('.stringify()', function () {
-  'use strict';
-
-  it('returns an empty string for undefined', function () {
-    expect(BRIXX.stringify()).to.be('');
-  });
-  it('returns an empty string for null', function () {
-    expect(BRIXX.stringify(null)).to.be('');
-  });
-  it('returns an empty string for NaN', function () {
-    expect(BRIXX.stringify(NaN)).to.be('');
-  });
-  it('returns "false" for false', function () {
-    expect(BRIXX.stringify(false)).to.be('false');
-  });
-  it('return "0" for 0', function () {
-    expect(BRIXX.stringify(0)).to.be('0');
-  });
-  it('returns "[object Function]" for function', function () {
-    var f = function () {};
-    expect(BRIXX.stringify(f)).to.be('[object Function]');
-  });
-  it('returns "[object Object]" for blank Object', function () {
-    expect(BRIXX.stringify({})).to.be('[object Object]');
-    expect(BRIXX.stringify(Object.create(null))).to.be('[object Object]');
-  });
-  it('returns the value of .toString() if it exists', function () {
-    var x = Object.create(null);
-    x.toString = function () {
-      return 'foo';
-    };
-    expect(BRIXX.stringify(x)).to.be('foo');
-  });
+	it('returns false when passed undefined', function () {
+		expect(BRIXX.exists()).to.be(false);
+	});
+	it('returns false when passed null', function () {
+		expect(BRIXX.exists(null)).to.be(false);
+	});
+	it('returns false when passed NaN', function () {
+		expect(BRIXX.exists(NaN)).to.be(false);
+	});
+	it('returns true when passed false', function () {
+		expect(BRIXX.exists(false)).to.be(true);
+	});
+	it('returns true when passed 0', function () {
+		expect(BRIXX.exists(0)).to.be(true);
+	});
+	it('returns true when passed ""', function () {
+		expect(BRIXX.exists('')).to.be(true);
+	});
+	it('returns true when passed function() {}', function () {
+		expect(BRIXX.exists(function () {})).to.be(true);
+	});
+	it('returns true when passed an Object', function () {
+		expect(BRIXX.exists({})).to.be(true);
+	});
+	it('returns true when passed an Array', function () {
+		expect(BRIXX.exists([])).to.be(true);
+	});
 });
